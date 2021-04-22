4cdb21cf6283ceea5e16dc882450798f4e13933e
MarcioMFS <00marciomendonca@gmail.com>
2021-04-22 14:51:38 -0300

    atualizações

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,14 @@
+
+        
+$('.banner').slick({
+infinite: true,
+slidesToShow: 1,
+slidesToScroll: 1,
+autoplay: true,
+autoplaySpeed: 5000,
+});
+
+
 document.querySelector(".menu-mobile").onclick = () => {
     document.documentElement.classList.add("menu-ativo");
   }
@@ -5,3 +16,10 @@ document.querySelector(".menu-mobile").onclick = () => {
   document.querySelector(".fechar-menu").onclick = () => {
     document.documentElement.classList.remove("menu-ativo");
   }
+
+
+  $("#sobre-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#sobre").offset().top
+    }, 2000);
+});
